dea2fdeb15004f1d7530f36ca6f61b6348c177d5
Siyam Mohammed <siyammoh11@gmail.com>
2023-06-19 16:44:42 +0300

    update deployment app to render

--- db/migrate/20230616201652_remove_articles_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveArticlesTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :articles
-  end
-end
--- db/migrate/20230616202210_remove_posts_table.rb
+++
@@ -1,5 +0,0 @@
-class RemovePostsTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :posts
-  end
-end
